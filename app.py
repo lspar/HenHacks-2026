b4cccf9a3d812713e7d306aec307ee6efcf62f20
Lindsey Spar <lspar@udel.edu>
2026-02-28 14:21:12 -0500

    starting dashboard code

--- app.py
+++ app.py
@@ -2,14 +2,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home_page():
-    profile = ""
-    if request.method == "POST":
+    return render_template("home.html")
+    
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+"""if request.method == "POST":
         email = request.form["email"]
         keywords = request.form.getlist("keywords")
         # Simulate processing the email
         profile = f"Saved email: {email}, Keywords: {keywords}"
         return render_template("home.html", result=profile)
-    return render_template("home.html")
-
+    return render_template("home.html")"""
