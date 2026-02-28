87cbc7c26247e1131985a64343c94c1e00416341
Lindsey Spar <lspar@udel.edu>
2026-02-28 16:04:01 -0500

    fixed commits

--- app.py
+++ app.py
@@ -1,18 +1,3 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route("/", methods=["GET", "POST"])
-def home_page():
-    profile = ""
-    if request.method == "POST":
-        email = request.form["email"]
-        keywords = request.form.getlist("keywords")
-        # Simulate processing the email
-        profile = f"Saved email: {email}, Keywords: {keywords}"
-        return render_template("home.html", result=profile)
-    return render_template("home.html")
-
 from flask import Flask, render_template, session, redirect, url_for
 from google_auth_oauthlib.flow import InstalledAppFlow
 
