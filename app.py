2b8411e774f633cb8ad9665b4e5caa7287a773c8
Lindsey Spar <lspar@udel.edu>
2026-02-28 13:20:02 -0500

    starting hompage code

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>IASSO</p>"
+@app.route("/", methods=["GET", "POST"])
+def home_page():
+    profile = ""
+    if request.method == "POST":
+        email = request.form["email"]
+        keywords = request.form.getlist("keywords")
+        # Simulate processing the email
+        profile = f"Saved email: {email}, Keywords: {keywords}"
+        return render_template("home.html", result=profile)
+    return render_template("home.html")
+
